df8c328a2a34b81fe5dbdec4458d46c6dbc87614
Andrei Blizuchenko <blizu4enko.andrei@yandex.ru>
2024-12-03 14:49:52 +0300

    Добавление 7 работы

---
+++ AppOC_WPF/Lb7_1Window.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace AppOC_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для Lb7_1Window.xaml
+    /// </summary>
+    public partial class Lb7_1Window : Window
+    {
+        private EventWaitHandle _eventWaitHandle;
+        public Lb7_1Window()
+        {
+            InitializeComponent();
+            _eventWaitHandle = new EventWaitHandle(false, EventResetMode.AutoReset, "InputEvent");
+            Thread controllerThread = new Thread(ControllerThread);
+            controllerThread.IsBackground = true;
+            controllerThread.Start();
+        }
+        private void ControllerThread()
+        {
+            while (true)
+            {
+                _eventWaitHandle.WaitOne(); // Ожидание сигнала от второго приложения
+                Dispatcher.Invoke(() => OutputTextBlock.Text += "*"); // Обновление UI
+            }
+        }
+
+    }
+}
---
+++ AppOC_WPF/Lb7_2Window.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AppOC_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для Lb7_2Window.xaml
+    /// </summary>
+    public partial class Lb7_2Window : Window
+    {
+        private EventWaitHandle _eventWaitHandle;
+        public Lb7_2Window()
+        {
+            InitializeComponent();
+            _eventWaitHandle = new EventWaitHandle(false, EventResetMode.AutoReset, "InputEvent");
+        }
+        private void InputTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter) // Игнорируем нажатие Enter
+            {
+                _eventWaitHandle.Set(); // Сигнализируем контроллеру о вводе символа
+            }
+        }
+    }
+}
